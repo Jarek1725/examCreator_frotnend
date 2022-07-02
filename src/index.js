e98fc5206b6b0f36ba6a4c1e666ca1473b6b2e2f
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-02 12:05:18 +0200

    Create IndexPage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import IndexPage from './components/indexPage/IndexPage'
 import reportWebVitals from './reportWebVitals';
+import {createTheme, ThemeProvider} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#f44336'
+        },
+        secondary: {
+            main: "#f44336"
+        }
+    }
+})
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+        <IndexPage/>
+    </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
