e85d09d43025aca5a38f3686f94898a43ae20f88
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-05 21:26:14 +0200

    Added grid elements to Browse Tab

--- src/components/mainGridElement/MainGridElement.js
+++ src/components/mainGridElement/MainGridElement.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './mainGridElement.scss'
+import {CircularProgress, Rating} from "@mui/material";
 
 const MainGridElement = (props) => {
 
@@ -43,24 +44,50 @@ const MainGridElement = (props) => {
                     ))}
             </div>
             <div className="main_grid_element_container_bottom" style={{
-                gridTemplateColumns: `repeat(${Object.keys(props.data).length-props.elementToShow}, 1fr)`
+                gridTemplateColumns: `repeat(${Object.keys(props.data).length - props.elementToShow}, 1fr)`
             }}>
                 {
                     Object.keys(props.data).map((key, index) => (
                         index >= props.elementToShow ?
                             (
-                                props.isFirstId && key === props.idName ?
-                                    <div className="single_grid_element_container_id">
-                                        <p className="single_grid_element_container_id"
-                                           style={{background: getRandomColor()}}>
-                                            {props.data[key]}
-                                        </p>
-                                    </div> :
-                                    <div className="single_grid_element_container">
-                                        <p className="single_grid_element_container_text">
-                                            {props.data[key]}
-                                        </p>
+                                <div className="single_grid_element_container_bottom">
+                                    <div className="single_grid_element_container_text">
+                                        {typeof props.data[key] == 'object' ?
+                                            <div className="single_grid_element_bottom_icons_container">
+                                                {props.data[key].ownType === 'average' ? (
+                                                        <>
+                                                            <CircularProgress variant="determinate"
+                                                                              value={props.data[key].value}/>
+                                                            <p className="single_grid_element_bottom_icons_text">{props.data[key].value}% average score</p>
+                                                        </>)
+                                                    : ('')
+                                                }
+                                                {props.data[key].ownType === 'rating' ? (
+                                                        <>
+                                                            <Rating name="half-rating" value={props.data[key].value} readOnly precision={0.5}  style={{color:"#5B59B5"}}/>
+                                                        </>)
+                                                    : ('')
+                                                }
+                                                {props.data[key].ownType === 'activeQuestions' ? (
+                                                        <>
+                                                            <p style={{background:"#C8EDA7", padding:"5px 13px", borderRadius:"7px", color:"white"}}>{props.data[key].value}</p>
+                                                            <p className="single_grid_element_bottom_icons_text">Active questions</p>
+                                                        </>)
+                                                    : ('')
+                                                }
+                                                {props.data[key].ownType === 'attempts' ? (
+                                                        <>
+                                                            <p style={{background:"#82A4F7", padding:"5px 13px", borderRadius:"7px", color:"white"}}>{props.data[key].value}</p>
+                                                            <p className="single_grid_element_bottom_icons_text">Active questions</p>
+                                                        </>)
+                                                    : ('')
+                                                }
+                                            </div>
+                                            :
+                                            <p>{props.data[key]}</p>
+                                        }
                                     </div>
+                                </div>
                             ) : ('')
                     ))}
             </div>
--- src/components/tabs/browse/BrowseTab.js
+++ src/components/tabs/browse/BrowseTab.js
@@ -51,65 +51,123 @@ const BrowseTab = () => {
 
     const gridData = [
         {
-            id:"#32FZ6H",
-            title:"Egzamin WIEIK z pierwszego semestru poprawkowy",
-            categories:"Programowanie, Sesja 2021",
-            lastUpdateDate:"03.07.2022",
-            averageScore:{
-                value:75,
-                icon:"TEST"
+            id: "#32FZ6H",
+            title: "Egzamin WIEIK z pierwszego semestru poprawkowy",
+            categories: "Programowanie, Sesja 2021",
+            lastUpdateDate: "03.07.2022",
+            averageScore: {
+                value: 75,
+                ownType: "average"
             },
-            rating:3.0,
-            activeQuestions:23,
-            attempts:67
+            rating: {
+                value:4,
+                ownType:"rating"
+            },
+            activeQuestions: {
+                value:23,
+                ownType:"activeQuestions"
+            },
+            attempts: {
+                value:23,
+                ownType:"attempts"
+            },
         },
         {
-            id:"#CR54A1",
-            title:"Bazy danych 06.2021",
-            categories:"Bazy danych, Sesja 2021",
-            lastUpdateDate:"14.06.2021",
-            // averageScore:63,
-            // rating:4.5,
-            // activeQuestions:17,
-            // attempts:18
+            id: "#CR54A1",
+            title: "Bazy danych 06.2021",
+            categories: "Bazy danych, Sesja 2021",
+            lastUpdateDate: "14.06.2021",
+            averageScore: {
+                value: 32,
+                ownType: "average"
+            },
+            rating: {
+                value:2.5,
+                ownType:"rating"
+            },
+            activeQuestions: {
+                value:12,
+                ownType:"activeQuestions"
+            },
+            attempts: {
+                value:44,
+                ownType:"attempts"
+            },
         },
         {
-            id:"#AV391K",
-            title:"Baza pytań PK WIEIK Architektura Systemów",
-            categories:"ASK, Sesja 2021",
-            lastUpdateDate:"17.06.2021",
-            // averageScore:63,
-            // rating:4.5,
-            // activeQuestions:17,
-            // attempts:18
+            id: "#AV391K",
+            title: "Baza pytań PK WIEIK Architektura Systemów",
+            categories: "ASK, Sesja 2021",
+            lastUpdateDate: "17.06.2021",
+            averageScore: {
+                value: 98,
+                ownType: "average"
+            },
+            rating: {
+                value:5,
+                ownType:"rating"
+            },
+            activeQuestions: {
+                value:22,
+                ownType:"activeQuestions"
+            },
+            attempts: {
+                value:76,
+                ownType:"attempts"
+            },
         },
         {
-            id:"#AV391K",
-            title:"Prawdopodobne pytania fizyka",
-            categories:"Fizyka, kolokwium zaliczeniowe",
-            lastUpdateDate:"17.01.2022",
-            // averageScore:63,
-            // rating:4.5,
-            // activeQuestions:17,
-            // attempts:18
+            id: "#AV391K",
+            title: "Prawdopodobne pytania fizyka",
+            categories: "Fizyka, kolokwium zaliczeniowe",
+            lastUpdateDate: "17.01.2022",
+            averageScore: {
+                value: 54,
+                ownType: "average"
+            },
+            rating: {
+                value:3.5,
+                ownType:"rating"
+            },
+            activeQuestions: {
+                value:28,
+                ownType:"activeQuestions"
+            },
+            attempts: {
+                value:13,
+                ownType:"attempts"
+            },
         },
         {
-            id:"#AV391K",
-            title:"Analiza matematyczna",
-            categories:"Matematyka, sesja 2022",
-            lastUpdateDate:"13.02.2022",
-            // averageScore:63,
-            // rating:4.5,
-            // activeQuestions:17,
-            // attempts:18
+            id: "#AV391K",
+            title: "Analiza matematyczna",
+            categories: "Matematyka, sesja 2022",
+            lastUpdateDate: "13.02.2022",
+            averageScore: {
+                value: 34,
+                ownType: "average"
+            },
+            rating: {
+                value:2.5,
+                ownType:"rating"
+            },
+            activeQuestions: {
+                value:52,
+                ownType:"activeQuestions"
+            },
+            attempts: {
+                value:76,
+                ownType:"attempts"
+            },
         }
     ]
 
     return (
         <div className="browse_tab_container">
             <SortByPanel data={sortByData()}/>
-            {gridData.map(data=>(
-                <MainGridElement data={data} widths={"1fr 5fr 2fr 2fr"} isFirstId={true} idName={"id"} elementToShow={4}/>
+            {gridData.map(data => (
+                <MainGridElement data={data} widths={"1fr 5fr 2fr 2fr"} isFirstId={true} idName={"id"}
+                                 elementToShow={4}/>
             ))}
         </div>
     );
