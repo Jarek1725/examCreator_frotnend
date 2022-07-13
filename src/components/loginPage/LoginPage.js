7a744def680ee88b483a201bf39e0808a715c703
Jarek1725 <tomaszewskijarek1725@gmail.com>
2022-07-14 00:50:31 +0200

    Add animation after wrong credentials

--- src/components/loginPage/LoginPage.js
+++ src/components/loginPage/LoginPage.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './loginPageStyle.scss'
-import {Button, CircularProgress, TextField} from "@mui/material";
+import {Alert, Button, CircularProgress, Snackbar, TextField} from "@mui/material";
 import {useQuery, gql, useLazyQuery, useMutation} from "@apollo/client";
 
 const CREATE_APP_USER = gql`
@@ -25,7 +25,20 @@ const LOGIN = gql`
 const LoginPage = () => {
     const [privateToken, setPrivateToken] = useState('')
 
-    let privateTokenTest = ''
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
 
     let handlePrivateTokenChange = (e) => {
         setPrivateToken(e)
@@ -37,31 +50,41 @@ const LoginPage = () => {
         data: createAppUserData,
         loading: createAppUserLoading
     }] = useMutation(CREATE_APP_USER)
-    const [login, {error: loginError, data: loginData, loading: loginoading}] = useLazyQuery(LOGIN)
+
+    const [login, {error: loginError, data: loginData, loading: loginLoading}] = useLazyQuery(LOGIN)
 
 
     let handleLogin = () => {
         if (privateToken.length > 0) {
-            console.log(privateTokenTest)
             login({
                 variables: {
                     privateToken
                 }
+            }).then(data => {
+                if (data.error) {
+                    document.querySelector("#login_page_error").style.opacity = 1
+                    setTimeout(function(){
+                        document.querySelector("#login_page_error").style.opacity = 0
+                    }, 3000)
+                }
             })
+
         } else {
             createAppUser()
         }
     }
 
 
-    if (createAppUserLoading) return <div className="login_page_container"><CircularProgress color="secondary"/></div>
+    if (createAppUserLoading || loginLoading) return <div className="login_page_container"><CircularProgress
+        color="secondary"/></div>
 
     return (
         <div className="login_page_container">
             <div className="login_page_input_text">
+                <p id="login_page_error">Cannot find user</p>
                 <h1>Login with token</h1>
-                <p>Input your private token to <span style={{color: "#5B59B5"}}>login</span> or leave blank to generate
-                    new</p>
+                <p>Input your private token to <span style={{color: "#5B59B5"}}>login</span> or leave blank <br/> to
+                    generate new</p>
                 <TextField id="standard-basic" label="Private token" variant="outlined" value={privateToken}
                            onChange={e => handlePrivateTokenChange(e.target.value)} size={"small"}
                            style={{width: 220, margin: "auto"}}/>
